9a0af076cd4aaa8510482c20dfb141ac466e92a4
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-24 20:51:54 -0300

    Create maior-entre-5-numeros.cpp

---
+++ estruturas-de-repeticao/maior-entre-5-numeros.cpp
@@ -0,0 +1,28 @@
+/*5. Faça um programa que leia 5 números e informe o maior número.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maiorn,numero;
+    
+    cout<<"digite um numero";
+    cin>>maiorn;
+    
+    for(int i=1; i<=4; i++){
+        
+        cout<<"digite 4 numeros"<<endl;
+        cin>>numero;
+        
+        if(numero>maiorn){
+            maiorn=numero;
+            
+        }
+    }
+    
+    cout<<"o maior numero e : "<<maiorn;
+    
+
+    return 0;
+}
