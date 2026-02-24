14c06bb4aef614ccb9856344fbd25e1104b55c79
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-24 20:48:22 -0300

    Create impressao-numeros-1-a-20.cpp

---
+++ estruturas-de-repeticao/impressao-numeros-1-a-20.cpp
@@ -0,0 +1,34 @@
+/*Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programapara que ele mostre os números um ao lado do outro.*/
+
+/*um abaixo do outro*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<21; i++){
+        
+        cout<<i<<endl;
+    }
+   
+    return 0;
+}
+
+
+/*
+um ao lado do outro
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1; i<21; i++){
+        
+        cout<<i<<" ";
+    }
+   
+    return 0;
+}*/
