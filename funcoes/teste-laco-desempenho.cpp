afa2050035048685553dc4bbcf232cde698ab1d0
Emerson Jesus <emersomjesus999@gmail.com>
2026-02-24 22:27:33 -0300

    Create teste-laco-desempenho.cpp

---
+++ funcoes/teste-laco-desempenho.cpp
@@ -0,0 +1,32 @@
+/*Escreva uma função que conte de até 1.000.000.000, isto é, a função deve conter um laço for
+com um contador que vai de 1 até 1.000.000.000. Escreva um programa que receba do teclado
+um valor, chame a função e então mostre o valor digitado.
+Exemplo de execução:
+Digite um número: 653
+Você digitou... 653*/
+
+#include <iostream>
+using namespace std;
+
+int contar(){
+    for(int i =1;i<=1000000000;i++){
+        
+    }
+}
+
+int main()
+{   int valor;
+
+    cout<<"digite um valor";
+    cin>>valor;
+    
+    contar();
+    
+    cout<<"o valor digitado e:"<<valor;
+    
+    
+    
+    
+
+    return 0;
+}
